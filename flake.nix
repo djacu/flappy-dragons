a3b16e08dfe7ef8f6c1ffb4b925af6e5ed0620c9
Daniel Baker <daniel.n.baker@gmail.com>
2024-04-04 17:37:22 -0700

    starting flappy dragons development.

--- flake.nix
+++ flake.nix
@@ -29,14 +29,31 @@
       in
       with pkgs;
       {
-        devShells.default = mkShell {
+        devShells.default = mkShell rec {
           buildInputs = [
             openssl
             pkg-config
-            eza
-            fd
+            fontconfig
             rust-bin.stable.latest.default
+
+            # Solves
+            # thread 'main' panicked at /home/bakerdn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/winit-0.27.5/src/platform_impl/linux/mod.rs:719:9:
+            # Failed to initialize any backend! Wayland status: NoCompositorListening X11 status: LibraryOpenError(OpenError { kind: Library, detail: "opening library failed (libX11.so.6: cannot open shared object file: No such file or directory); opening library failed (libX11.so: cannot open shared object file: No such file or directory)" })
+            # note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+            libxkbcommon
+            libGL
+
+            # WINIT_UNIX_BACKEND=wayland
+            wayland
+
+            # WINIT_UNIX_BACKEND=x11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+            xorg.libX11
           ];
+
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
         };
       }
     );
